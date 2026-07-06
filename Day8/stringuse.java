8bc0529fb26dd5a90f30b0a5d4e08de40b686f91
Antopriya17 <antopriya2007@gmail.com>
2026-07-06 21:11:16 +0530

    Add files via upload

---
+++ Day8/stringuse.java
@@ -0,0 +1,24 @@
+package Day8;
+
+public class stringuse {
+    public static void main(String[] args) {
+        String str="hEllo";
+        String Strg=str.toLowerCase();
+        int countv=0;
+        int countc=0;
+        char[] ch=Strg.toCharArray();
+        for(char num:ch){
+            if(num=='a'||num=='e'||num=='i'||num=='o'||num=='u'){
+                countv++;
+            }
+            else{
+                countc++;
+            }
+
+        }
+        System.out.println("number of vowels:"+countv);
+        System.out.println("Numberof consonats:"+countc);
+    }
+}
+
+
