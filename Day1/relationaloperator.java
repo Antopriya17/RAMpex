f41d372ed59e75f23b513393363413ce01246868
Antopriya17 <antopriya2007@gmail.com>
2026-07-06 13:12:41 +0530

    Add files via upload

---
+++ Day1/relationaloperator.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class relationaloperator {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+    }
+}
